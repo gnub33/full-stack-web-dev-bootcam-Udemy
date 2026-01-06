8b222aff3c55980ce11647fcf8780cacff697f77
dknight <dknight@code.wm.edu>
2026-01-05 22:17:53 -0500

    small, simple intro to jquery

---
+++ jQuery/index.js
@@ -0,0 +1,12 @@
+$("h1").css("font-family", "arial");
+
+
+
+$("button").css("background-color", "yellow");
+
+$("p").addClass("bold-beautiful");
+
+$("button").click(function() {
+    $("h1").css("color","red");
+    $("h1").text("YOU CLICKED ME AHHH!");
+})
