1fe1f7248373e52952ecebe74291cef52f47e5b5
dknight <gnub33@gmail.com>
2026-09-11 19:51:55 -0400

    very long log

---
+++ Backend/capstone-blog/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser"
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded());
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/about", (req, res) => {
+    res.send("about.ejs");
+})
+
+app.get("/settings", (req, res) => {
+    res.send("settings.ejs");
+})
+app.get("/help", (req, res) => {
+    res.send("help.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
