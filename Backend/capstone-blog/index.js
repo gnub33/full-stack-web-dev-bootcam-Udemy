70865cf229c96b7296542337386622548545a23e
dknight <gnub33@gmail.com>
2026-09-11 21:59:04 -0400

    root page renders

--- Backend/capstone-blog/index.js
+++ Backend/capstone-blog/index.js
@@ -3,25 +3,35 @@ import bodyParser from "body-parser"
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {name :'hop'});
 })
 
-app.get("/about", (req, res) => {
-    res.send("about.ejs");
-})
+// app.get("/about", (req, res) => {
+//     res.render("about.ejs");
+// })
 
-app.get("/settings", (req, res) => {
-    res.send("settings.ejs");
-})
-app.get("/help", (req, res) => {
-    res.send("help.ejs");
+// app.get("/settings", (req, res) => {
+//     res.render("settings.ejs");
+// })
+
+// app.get("/help", (req, res) => {
+//     res.render("help.ejs");
+// })
+
+app.post("/login", (req, res) => {
+    const user = "email";
+    const pass = "0000";
+    res.render("home.ejs", {username: user, password: pass});
+    console.log(req.body);
 })
 
 app.post("/submit", (req, res) => {
-    res.send("index.ejs", )
+    res.render("home.ejs" );
+    console.log(req.body);
 })
 
 app.listen(port, () => {
