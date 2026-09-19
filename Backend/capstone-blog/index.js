2e32b476eb60ba896801d3df9de77d64d7a19171
dknight <gnub33@gmail.com>
2026-09-18 21:44:55 -0400

    delete button

--- Backend/capstone-blog/index.js
+++ Backend/capstone-blog/index.js
@@ -7,8 +7,9 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {name :'hop'});
+    res.render("index.ejs", {usr :'p_nut', latest: allBlogs});
 })
+
 
 // app.get("/about", (req, res) => {
 //     res.render("about.ejs");
@@ -22,27 +23,37 @@ app.get("/", (req, res) => {
 //     res.render("help.ejs");
 // })
 
-app.post("/login", (req, res) => {
+app.post("/home", (req, res) => {
+    //for login page
     const user = req.body["username"];
     const password = req.body["password"];
 
-    res.render("home.ejs", {usr: user, pass: password});
+    res.render("index.ejs", {usr: user, pass: password});
     console.log(req.body);
 })
 
-app.post("/submit", (req, res) => {
-    const blgpst = req.body["blgpst"]
+app.post("/submit/post", (req, res) => {
+   
+    const blgpst = {id: allBlogs.length + 1, content:req.body.postItem} // may need to change to let??
     
-    blogs.push(blgpst);
+    allBlogs.push(blgpst);
     
-    res.render("home.ejs", {latest : blogs});
+    res.render("index.ejs", {latest : allBlogs});
+
+    console.log()
 })
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
 
-const blogs = [];
+app.post("/posts/:id/delete", (req, res) => {
+    const id = parseInt(req.params.id);
+    allBlogs = allBlogs.filter(post => post.id !== id);
+    res.redirect("/"); 
+})
+
+let allBlogs = [];
 
 function confirm() {
     var txt;
@@ -53,3 +64,12 @@ function confirm() {
         pass;
     }
 }
+
+function addButtons(index, array) {
+    let updateBtn = document.createElementButton("button");
+    updateBtn.innerHTML = "Edit";
+
+    let delBtn = document.createElementButton("button");
+    delBtn.innerHTML = "Delete"
+}
+
